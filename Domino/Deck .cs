e5e4a43af5e6e02df46b2eea0e82ee26437c1f80
aziz56 <aziz.abdulaziz99@gmail.com>
2023-09-07 13:58:46 +0700

    EditDeck

--- Domino/Deck .cs
+++ Domino/Deck .cs
@@ -1,25 +1,28 @@
  namespace Domino;
     public class Deck
     {
-        private List<Tile> _tiles = new List<Tile>();
+        private List<Tile> _tilesDeck = new List<Tile>();
         private int _totalSide;
         public Deck(int totalSide)
         {
-
+            _totalSide = totalSide;
+            _tilesDeck = new List<Tile>();
+            CreateTiles();
+            Shuffle();
         }
         public void CreateTiles()
         {
             HashSet<Tile> uniqueTiles = new HashSet<Tile>();
 
-            for (int a = 0; a <= 6; a++)
+            for (int side1 = 0; side1 <= _totalSide; side1++)
             {
-                for (int b = a; b <= 6; b++)
+                for (int side2 = side1; side2 <= 6; b++)
                 {
-                    Tile tile = new Tile(a, b);
+                    Tile tile = new Tile(side1, side2);
                     
                     if (uniqueTiles.Add(tile))
                     {
-                        _tiles.Add(tile);
+                        _tilesDeck.Add(tile);
                     }
                 }
             }
@@ -27,17 +30,17 @@
             //buat method dengan return valuenya dia berhasil atau tidak
         }
 
-        // public void Shuffle()
-        // {
-        //     Random rng = new Random();
-        //     int n = _tiles.Count;
-        //     while (n > 1)
-        //     {
-        //         n--;
-        //         int k = rng.Next(n + 1);
-        //         Tile value = _tiles[k];
-        //         _tiles[k] = _tiles[n];
-        //         _tiles[n] = value;
-        //     }
-        // }
+        public void Shuffle()
+        {
+            Random rng = new Random();
+            int n = _tilesDeck.Count;
+            while (n > 1)
+            {
+                n--;
+                int indexRand = rng.Next(n + 1);
+                Tile value = _tilesDeck[indexRand];
+                _tilesDeck[indexRand] = _tilesDeck[n];
+                _tilesDeck[n] = value;
+            }
+        }
     }
